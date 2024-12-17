105cec5dc57ec14118261fe80db58c4ac822bb8b
Eugene000000 <rsergeevoifnaf1@gmail.com>
2024-12-17 14:14:39 +0500

    Update google_click_button.py

--- lesson_05/google_click_button.py
+++ lesson_05/google_click_button.py
@@ -10,7 +10,7 @@ driver = webdriver.Chrome(service = ChromeService(ChromeDriverManager().install(
 driver.get("http://the-internet.herokuapp.com/add_remove_elements/")
 
 for i in range(5):
-    and_button = driver.find_element(By.XPATH, ('//button[text()="addElement"]')).click()
+    and_button = driver.find_element(By.XPATH, ('//button[text()="Add Element"]')).click()
     
     chrome_delete_buttons = driver.find_elements(By.XPATH, ('//button[text()="Delete"]'))
     
