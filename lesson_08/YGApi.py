957912d4d12d9224d895acda4252a31b5b2376c0
Eugene000000 <rsergeevoifnaf1@gmail.com>
2025-01-23 18:14:38 +0500

    Add files via upload

---
+++ lesson_08/YGApi.py
@@ -0,0 +1,42 @@
+import requests
+
+class YG():
+
+    def __init__(self, url):
+        self.url = url
+    
+    def get_project_list(self, token='Bearer j-iDnUeA48Hi2lIiI--lNoWZLT3AB4bxyV953mWcMoTXxUYuv4IoKn5Mi7H2P96v'):
+        my_headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer undefined"
+        }
+        my_headers["Authorization"] = token
+        result = requests.get(self.url + '/api-v2/projects', headers=my_headers)
+        return result
+
+    def add_project(self, title):
+        my_headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer j-iDnUeA48Hi2lIiI--lNoWZLT3AB4bxyV953mWcMoTXxUYuv4IoKn5Mi7H2P96v"
+           }
+        body = {"title": title}
+        result = requests.post(self.url + '/api-v2/projects', json=body, headers=my_headers)
+        return result
+
+    def get_project_with_id(self, id):
+        my_headers = {
+             "Content-Type": "application/json",
+             "Authorization": "Bearer j-iDnUeA48Hi2lIiI--lNoWZLT3AB4bxyV953mWcMoTXxUYuv4IoKn5Mi7H2P96v"
+        }
+        result = requests.get(self.url + '/api-v2/projects/' + id, headers=my_headers)
+        return result
+
+    def change_project_title(self, id, new_title, token='Bearer j-iDnUeA48Hi2lIiI--lNoWZLT3AB4bxyV953mWcMoTXxUYuv4IoKn5Mi7H2P96v'):
+        my_headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer j-iDnUeA48Hi2lIiI--lNoWZLT3AB4bxyV953mWcMoTXxUYuv4IoKn5Mi7H2P96v"
+        }
+        my_headers["Authorization"] = token
+        body = {'title': new_title}
+        result = requests.put(self.url + '/api-v2/projects/' + id, json=body, headers=my_headers)
+        return result
