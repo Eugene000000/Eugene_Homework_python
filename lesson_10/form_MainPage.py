e3840019f6480e16c819aed47b147adf15ed9ea2
Eugene000000 <rsergeevoifnaf1@gmail.com>
2025-02-02 18:36:56 +0500

    Add files via upload

---
+++ lesson_10/form_MainPage.py
@@ -0,0 +1,118 @@
+import allure
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+class MainPage:
+
+    """Класс с методами для тестирования формы"""
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.url = "https://bonigarcia.dev/selenium-webdriver-java/data-types.html"
+
+        self._first_name = (By.NAME, "first-name")
+        self._last_name = (By.NAME, "last-name")
+        self._address = (By.NAME, "address")
+        self._email = (By.NAME, "e-mail")
+        self._phone = (By.NAME, "phone")
+        self._zip_code = (By.NAME, "zip-code")
+        self._city = (By.NAME, "city")
+        self._country = (By.NAME, "country")
+        self._job_position = (By.NAME, "job-position")
+        self._company = (By.NAME, "company")
+        self._submit_button = (By.TAG_NAME, "button")
+
+with allure.step("Открыть страницу с формой"):
+    def open(self):
+        self.driver.get(self.url)
+with allure.step("Заполнить форму"):
+    def fill_form(self, first_name: str, last_name: str, address: str, email: str, phone: str, zip_code: str, city: str, country: str, job_position: str, company: str) -> None:
+     with allure.step("Заполнить поле 'First name'"):
+        self.driver.find_element(*self._first_name).send_keys(first_name)
+        """
+        Функция находит поле "First name" и заполняет его 
+        """
+
+     with allure.step("Заполнить поле 'Last name'"):
+        self.driver.find_element(*self._last_name).send_keys(last_name)
+        """
+        Функция находит поле "Last name" и заполняет его 
+        """
+     with allure.step("Заполнить поле 'Address'"):
+        self.driver.find_element(*self._address).send_keys(address)
+        """
+        Функция находит поле "address" и заполняет его 
+        """
+     with allure.step("Заполнить поле 'Email'"):
+        self.driver.find_element(*self._email).send_keys(email)
+        """
+        Функция находит поле "email" и заполняет его 
+        """
+     with allure.step("Заполнить поле 'Phone'"):
+        self.driver.find_element(*self._phone).send_keys(phone)
+        """
+        Функция находит поле "phone" и заполняет его 
+        """
+     with allure.step("Не заполнить поле 'Zip_code'"):
+        self.driver.find_element(*self._zip_code).send_keys(zip_code)
+        """
+        Функция находит поле "zip_code" и не заполняет его 
+        """
+     with allure.step("Заполнить поле 'City'"):
+        self.driver.find_element(*self._city).send_keys(city)
+        """
+        Функция находит поле "city" и не заполняет его 
+        """
+     with allure.step("Заполнить поле 'Country'"):
+        self.driver.find_element(*self._country).send_keys(country)
+        """
+        Функция находит поле "country" и не заполняет его 
+        """
+     with allure.step("Заполнить поле 'Job'"):
+        self.driver.find_element(*self._job_position).send_keys(job_position)
+        """
+        Функция находит поле "job_position" и не заполняет его 
+        """
+     with allure.step("Заполнить поле 'Company'"):
+        self.driver.find_element(*self._company).send_keys(company)
+        """
+        Функция находит поле "company" и не заполняет его 
+        """
+        
+with allure.step("Нажатие на кнопку SUBMIT"):
+    def submit(self, submit_button: str) -> None:
+        self.driver.find_element(*self._submit_button).click()
+        """
+        Эта функция для нажимания кнопки "Submit"
+        """
+with allure.step("Проверка цвета поля ZIP_CODE, оно красное"):
+    def get_zip_code_field_color(self) -> None:
+        self._zip_code = (By.ID, "zip-code")
+        return self.driver.find_element(*self._zip_code).value_of_css_property('border-color')  # Проверка цвета поля
+    
+with allure.step("Проверка цветов других полей, они зелёные"):
+    def get_other_fields_color(self) -> None:
+        self._first_name = (By.ID, "first-name")
+        self._last_name = (By.ID, "last-name")
+        self._address = (By.ID, "address")
+        self._email = (By.ID, "e-mail")
+        self._phone = (By.ID, "phone")
+        self._city = (By.ID, "city")
+        self._country = (By.ID, "country")
+        self._job_position = (By.ID, "job-position")
+        self._company = (By.ID, "company")
+        """
+        Функция для проверки цветов заполненных полей, а именно в зеленый цвет:
+        """
+        
+        return {
+            'first_name': self.driver.find_element(*self._first_name).value_of_css_property('border-color'),
+            'last_name': self.driver.find_element(*self._last_name).value_of_css_property('border-color'),
+            'address': self.driver.find_element(*self._address).value_of_css_property('border-color'),
+            'email': self.driver.find_element(*self._email).value_of_css_property('border-color'),
+            'phone': self.driver.find_element(*self._phone).value_of_css_property('border-color'),
+            'city': self.driver.find_element(*self._city).value_of_css_property('border-color'),
+            'country': self.driver.find_element(*self._country).value_of_css_property('border-color'),
+            'job_position': self.driver.find_element(*self._job_position).value_of_css_property('border-color'),
+            'company': self.driver.find_element(*self._company).value_of_css_property('border-color'),
+        }
---
+++ lesson_10/shop_MainPage.py
@@ -0,0 +1,62 @@
+import allure
+from selenium.webdriver.common.by import By
+
+class ShopPage:
+    """
+    Класс с методами для тестирования магазина
+    """
+    def __init__(self, driver):
+        self.driver = driver
+        self.url = "https://www.saucedemo.com/"
+
+        self.username = (By.ID, "user-name")
+        self.password = (By.ID, "password")
+        self.login_button = (By.CSS_SELECTOR, ".btn_action") 
+        self.backpack = (By.ID, "add-to-cart-sauce-labs-backpack")
+        self.tshirt = (By.ID, "add-to-cart-sauce-labs-bolt-t-shirt")
+        self.onesie = (By.ID, "add-to-cart-sauce-labs-onesie")
+        self.cart_button = (By.CSS_SELECTOR, ".shopping_cart_link")  
+        self.checkout_button = (By.CSS_SELECTOR, "#checkout")
+        self.first_name = (By.ID, "first-name")
+        self.last_name = (By.ID, "last-name")
+        self.postal_code = (By.ID, "postal-code")
+        self.continue_button = (By.CSS_SELECTOR, ".btn_primary.cart_button") 
+        self.total = (By.CSS_SELECTOR, ".summary_total_label") 
+
+with allure.step("Открыть страницу магазина"):
+    def open(self):
+        self.driver.get(self.url)
+
+with allure.step("Вход"):
+    def login(self, username, password):
+        self.driver.find_element(*self.username).send_keys(username)
+        self.driver.find_element(*self.password).send_keys(password)
+        self.driver.find_element(*self.login_button).click()
+
+with allure.step("Добавление товаров в корзину"):
+    def add_to_cart(self):
+        """
+        Добавление трёх товаров из магазина
+        """
+        self.driver.find_element(*self.backpack).click()
+        self.driver.find_element(*self.tshirt).click()
+        self.driver.find_element(*self.onesie).click()
+
+with allure.step("Введение данных и оплата"):
+    def checkout(self, first_name, last_name, postal_code):
+        """
+        Введение данных и оплата
+        """
+        self.driver.find_element(*self.cart_button).click()
+        self.driver.find_element(*self.checkout_button).click()
+        self.driver.find_element(*self.first_name).send_keys(first_name)
+        self.driver.find_element(*self.last_name).send_keys(last_name)
+        self.driver.find_element(*self.postal_code).send_keys(postal_code)
+        self.driver.find_element(*self.continue_button).click()
+
+with allure.step("Проверка совпадения цены"):
+    def get_total(self):
+        """
+        Проверка совпадения цены
+        """
+        return self.driver.find_element(*self.total).text
