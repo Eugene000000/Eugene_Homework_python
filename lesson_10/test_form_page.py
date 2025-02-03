4930e2d987f4467f9aee7c50c731f70142b9823a
Eugene000000 <rsergeevoifnaf1@gmail.com>
2025-02-03 17:53:17 +0500

    Add files via upload

---
+++ lesson_10/test_form_page.py
@@ -0,0 +1,46 @@
+import allure
+import pytest
+from selenium import webdriver
+from form_MainPage import MainPage
+
+@pytest.fixture
+def chrome_browser():
+        driver = webdriver.Chrome()  
+        yield driver
+        driver.quit()
+
+@allure.id("lesson_10")
+@allure.story("Заполнение формы")
+@allure.feature("CREATE")
+@allure.epic("Форма")
+@allure.title("Проверка цветов")
+@allure.description("Проверка выделения красным цветом поля 'Zip code' при его незаполнении")
+@allure.severity("blocker")
+def test_fill_form(chrome_browser):
+        form_page = MainPage(chrome_browser)
+        form_page.open()
+    
+        form_page.fill_form(
+        "Иван", 
+        "Петров", 
+        "Ленина, 55-3", 
+        "test@skypro.com", 
+        "+7985899998787", 
+        "", 
+        "Москва", 
+        "Россия", 
+        "QA", 
+        "SkyPro"
+        )
+        form_page.submit()
+
+        assert form_page.get_zip_code_field_color() == 'rgb(245, 194, 199)'
+        colors = form_page.get_other_fields_color()
+        for color in colors.values():
+            assert color == 'rgb(186, 219, 204)'      
+
+
+
+
+
+   
